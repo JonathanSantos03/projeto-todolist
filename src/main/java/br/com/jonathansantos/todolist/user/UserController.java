942c9865a8efe3449f6702f6783356a488bb8013
Jonathan Santos <jonathan.sousa.df@gmail.com>
2025-02-25 17:27:02 -0300

    Implementação bcrypt para armazenar senhas de forma segura no banco de dados

--- src/main/java/br/com/jonathansantos/todolist/user/UserController.java
+++ src/main/java/br/com/jonathansantos/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.jonathansantos.todolist.user;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 /**
  * Modificador
@@ -44,6 +45,11 @@ public class UserController {
             // Status code
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashred = BCrypt.withDefaults()
+        .hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
