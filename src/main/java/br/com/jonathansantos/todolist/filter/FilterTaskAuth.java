fe655838bf59e1326ade65f4018b520aa501943e
Jonathan Santos <jonathan.sousa.df@gmail.com>
2025-02-26 10:45:03 -0300

    adicionando decode da senha

--- src/main/java/br/com/jonathansantos/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/jonathansantos/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.jonathansantos.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.jonathansantos.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 
@@ -14,6 +17,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -37,11 +43,21 @@ public class FilterTaskAuth extends OncePerRequestFilter {
                 System.out.println(password);
                 
                 // Validar usuário
+                var user = this.userRepository.findByUsername(username);
+                if (user == null) {
+                    response.sendError(401);
+                } else {
+                    // Validar senha
+                    var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                    if (passwordVerify.verified) {
+                        filterChain.doFilter(request, response);
+                    } else {
+                        response.sendError(401);
+                    }
 
-                // Validar senha
-
-                // Segue em frente
-                filterChain.doFilter(request, response);
+                    // Segue em frente
+                    filterChain.doFilter(request, response);
+                }
     }
 
 }
